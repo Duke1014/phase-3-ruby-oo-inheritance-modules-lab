c4d98e507e20819575be1e757117f8c014cc979d
duke1014 <duke.norsworthy@gmail.com>
2021-11-02 22:57:02 -0400

    commit tree 13a1a5d4a7182a9d19e4f383ae160d605889c880

---
+++ lib/concerns/findable.rb
@@ -0,0 +1,14 @@
+require 'pry'
+
+module Findable
+    module InstanceMethods
+    end
+
+    module ClassMethods
+        
+        def find_by_name(name)
+            self.all.detect{|a| a.name == name}
+        end
+        
+    end
+end
